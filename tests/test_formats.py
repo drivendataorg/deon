d8bc1606f0275dfd46a678c7c362bc0f704163f1
ejm714 <ejm714@gmail.com>
2018-08-09 10:16:50 -0700

    jupyter notebook test

--- tests/test_formats.py
+++ tests/test_formats.py
@@ -1,7 +1,9 @@
 from pytest import fixture
+from pathlib import Path
+import json
 
 from ethics_checklist.parser import Checklist, Section
-from ethics_checklist.formats import Markdown
+from ethics_checklist.formats import Markdown, JupyterNotebook
 
 
 @fixture
@@ -13,11 +15,12 @@ def checklist():
 
 
 def test_format():
+    # does this need to be tested separately from markdown?
     pass
 
 
 def test_markdown(checklist, tmpdir):
-    known_good = '# My Checklist\n\n## Section 1\n------\n - [ ] first\n - [ ] '\
+    known_good = '# My Checklist\n\n## Section 1\n------\n - [ ] first\n - [ ] ' \
                  'second\n\n## Section 2\n------\n - [ ] third\n - [ ] fourth\n\n'
 
     m = Markdown(checklist)
@@ -28,5 +31,28 @@ def test_markdown(checklist, tmpdir):
     assert temp_file_path.read() == known_good
 
 
-def test_jupyter():
-    pass
+def test_jupyter(checklist, tmpdir):
+    known_good = ({'cell_type': 'markdown',
+                  'metadata': {},
+                   'source': ['# My Checklist\n',
+                               '\n',
+                               '## Section 1\n',
+                              '------\n',
+                              ' - [ ] first\n',
+                              ' - [ ] second\n',
+                              '\n',
+                              '## Section 2\n',
+                              '------\n',
+                              ' - [ ] third\n',
+                              ' - [ ] fourth\n',
+                              '\n',
+                              '\n']})
+
+    j = JupyterNotebook(checklist)
+    assert j.render() == known_good
+
+    temp_file_path = tmpdir.join('test.ipynb')
+    j.write(temp_file_path, overwrite=True)
+    with open(temp_file_path, 'r') as f:
+        nbdata = json.load(f)
+    assert nbdata['cells'] == [known_good]
