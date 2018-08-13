64d1a374015cdd43adeafb4a0598c6a1df9347a4
ejm714 <ejm714@gmail.com>
2018-08-13 16:47:10 -0700

    update test due to new checklist

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -4,6 +4,7 @@ from ethics_checklist.parser import Checklist
 def test_checklist():
     c = Checklist.read('checklist.yml')
     assert c.title == 'Data Science Ethics Checklist'
-    assert [s.title for s in c.sections] == ['Data Collection', 'Exploratory Analysis']
+    assert [s.title for s in c.sections][2] == 'Data Storage'
     lines = [s.lines for s in c.sections]
-    assert lines[1][1] == 'Fields with PII are not used or displayed unless necessary for the analysis.'
+    assert lines[1][1] == 'Have we considered sources of bias that could be introduced during ' \
+                          'data collection and survey design and taken steps to mitigate those?'
