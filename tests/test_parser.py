f9ef61db0b3addcd00b9a1fc27ef375e39f0a70e
Peter Bull <pjbull@gmail.com>
2018-08-14 13:13:53 -0700

    Update checklist based on feedback (#20)
    * Update checklist based on feedback
    
    * Stop hardcoded tests

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -1,10 +1,14 @@
+import yaml
+
 from ethics_checklist.parser import Checklist
 
 
 def test_checklist():
     c = Checklist.read('checklist.yml')
-    assert c.title == 'Data Science Ethics Checklist'
-    assert [s.title for s in c.sections][2] == 'Data Storage'
+    with open('checklist.yml', "r") as f:
+        raw_parsed = yaml.load(f)
+
+    assert c.title == raw_parsed['title']
+    assert [s.title for s in c.sections][2] == raw_parsed['sections'][2]['title']
     lines = [s.lines for s in c.sections]
-    assert lines[1][1] == 'Have we considered sources of bias that could be introduced during ' \
-                          'data collection and survey design and taken steps to mitigate those?'
+    assert lines[1][1] == raw_parsed['sections'][1]['lines'][1]
