d3749cecf6df4b29c03327acb67839b0ee37b5d9
ejm714 <ejm714@gmail.com>
2018-08-15 18:08:36 -0700

    generate markdown table from reference links

---
+++ ethics_checklist/utils.py
@@ -0,0 +1,40 @@
+from pathlib import Path
+
+import yaml
+
+# from parser import Checklist
+
+
+def make_table_of_links():
+    root = Path(__file__).parents[1]
+    # cl = Checklist.read(root / 'checklist.yml')
+
+    with open(root / 'references.yml', 'r') as f:
+        refs = yaml.load(f)
+
+    template = """Question | Link
+--- | ---
+{lines}
+"""
+    line_template = "{line_id} | {hyperlink}"
+    line_delimiter = "\n"
+
+    formatted_rows = []
+    for checklist_item in refs:
+        line_id = checklist_item['line_id']
+
+        for link in checklist_item['links']:
+            text = link['text']
+            url = link['url']
+
+            row = (line_template.format(
+                    line_id=line_id,
+                    hyperlink=f"[{text}]({url})"))
+            formatted_rows.append(row)
+
+    all_rows = line_delimiter.join(formatted_rows)
+    return template.format(lines=all_rows)
+
+
+if __name__ == '__main__':
+    make_table_of_links()
