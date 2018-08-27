d063e246db3a205f7e0c974c2d013545127daea0
ejm714 <ejm714@gmail.com>
2018-08-27 15:37:43 -0700

    include line summaries in table of examples

--- docs/render_templates.py
+++ docs/render_templates.py
@@ -41,55 +41,46 @@ def create_context():
 
 def make_table_of_links():
     root = Path(__file__).absolute().parents[1]
+
     cl = Checklist.read(root / 'checklist.yml')
-
-    # create dictionary of line ids and checklist questions
-    question_dict = dict()
-    for s in cl.sections:
-        for l in s.lines:
-            question_dict[l.line_id] = l.line
-
-    section_dict = dict()
-    for s in cl.sections:
-        section_dict[s.section_id] = s.title
 
     with open(root / 'examples.yml', 'r') as f:
         refs = yaml.load(f)
+
+    refs_dict = dict()
+    for r in refs:
+        refs_dict[r['line_id']] = r['links']
 
     template = """<center>Checklist Question</center> | <center>Examples of Ethical Issues</center>
 --- | ---
 {lines}
 """
-    line_template = "{line_id} | {row_text}"
+    line_template = "**{line_id} {line_summary}**: {line} | {row_text}"
+    section_title_template = " | <center>**{section_title}**</center>"
     line_delimiter = "\n"
 
     formatted_rows = []
-    for checklist_item in refs:
-
-        line_id = checklist_item['line_id']
-        question = question_dict[line_id]
-
-        # if have first item of a section, first include section title
-        if line_id.split('.')[1] == '1':
-            # break
-            section_title = section_dict[line_id.split('.')[0]]
-            row = line_template.format(line_id='',
-                                       row_text=f"<center>**{section_title}**</center>")
-            formatted_rows.append(row)
-
-        bulleted_list = []
-        for i, link in enumerate(checklist_item['links']):
-            text = link['text']
-            url = link['url']
-            bullet_hyperlink = f"<li>[{text}]({url})</li>"
-            bulleted_list.append(bullet_hyperlink)
-        formatted_bullets = ''.join(bulleted_list)
-
-        row = (line_template.format(
-                line_id=f"**{line_id}**. {question}",
-                row_text=f"<ul>{formatted_bullets}</ul>"))
+    for s in cl.sections:
+        # section title row
+        row = section_title_template.format(section_title=s.title)
         formatted_rows.append(row)
 
+        for l in s.lines:
+            # create bulleted list of links for each checklist item in that section
+            bulleted_list = []
+            for i, link in enumerate(refs_dict[l.line_id]):
+                text = link['text']
+                url = link['url']
+                bullet_hyperlink = f"<li>[{text}]({url})</li>"
+                bulleted_list.append(bullet_hyperlink)
+            formatted_bullets = ''.join(bulleted_list)
+
+            row = (line_template.format(
+                    line_id=l.line_id, line_summary=l.line_summary, line=l.line,
+                    row_text=f"<ul>{formatted_bullets}</ul>"))
+            formatted_rows.append(row)
+
+    # bring all the rows together
     all_rows = line_delimiter.join(formatted_rows)
     return template.format(lines=all_rows)
 
