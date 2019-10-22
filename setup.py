b8be85b54b46aec4f019e20039fe64d3dc72bfaf
Peter Bull <peter@drivendata.org>
2019-10-21 21:39:21 -0700

    Bump version number

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ setup(
         'Source Code': 'https://github.com/drivendataorg/deon',
         'DrivenData': 'http://drivendata.co'
     },
-    version='0.1.3',
+    version='0.1.4',
     author="DrivenData",
     author_email="info@drivendata.org",
 
